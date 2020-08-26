cf5a2f86635425ed70aaf2ee61817298220b2385
Juan Zaragoza <56614909+ItsJuanito@users.noreply.github.com>
2020-08-25 21:02:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,69 @@
-$.ajax({ url:"https://api.nasa.gov/planetary/apod?api_key=PWhhelBrT1WPUSF5ujClLDHxZV3nMMT4U8Kxz3b9",
-success: function(data) {
 
+const api = {
+    key: "https://api.nasa.gov/planetary/apod?",
+    base: "PWhhelBrT1WPUSF5ujClLDHxZV3nMMT4U8Kxz3b9"
+} // api key and base
+
+const searchBox = document.querySelector('.form-control');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchBox.value);
+        console.log(searchBox.value);
+    } //presses the enter key then calls getResult function
+}
+
+function getResults(query) {
+    fetch(`https://images-api.nasa.gov/search?q=${query}`)
+    .then(data => {
+        return data.json();
+    }).then(displayResults);
+} // fetches api and gets results and query then displayResults is executed
+
+function displayResults(data) {
     console.log(data);
 
-    const info = document.getElementById('info');
-    if (data.media_type == "video") {
+    document.write(`<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"`);
+    document.write(`
+        <body>
+            <div id="in" class="py-2 container">
+            </div>`
+    );
 
-        info.innerHTML = 
-        `<iframe src="${data.url}" width="800px" height="500px">
-        </iframe>
+    let input = document.getElementById('in').innerHTML = `
+        <h1 class="d-flex justify-content-center">NASA's Image of the Day</h1>
+        <input class="form-control" id="input" autocomplete="off" type="text" placeholder="Search for an image...">`;
 
-        <h2 id="title">${data.title}</h2>
-        <h4 id="copyright">By ${data.copyright}</h4>
-        <p id="explanation">${data.explanation}</p>`;
-    } else if (data.media_type == "image") {
-        document.getElementById('info').innerHTML = `<img id="img" src="${data.url}" class="rounded" alt="Cinque Terre" width="75%" height="30%">`;
+    let size = data.collection.items;
+
+    for (let i = 0; i < size.length; i++) {
+            document.write(
+                `<div class="container">
+                    <div class="card">
+                        <div class="card-body">
+                            <img class="mt-2 rounded" id="img" src="${data.collection.items[i].links[0].href}"/>
+        
+                            <h2 class="my-3 card-title" id="title">${data.collection.items[i].data[0].title}</h2>
+                            <h4 class="my-3" id="copyright">By ${data.collection.items[i].data[0].photographer}</h4>
+                            <p class="my-3 card-text" id="location">Location: ${data.collection.items[i].data[0].location}</p>
+                        </div>
+                    </div>
+                </div>`);
     }
 
-    if (data.copyright === undefined) {
-            document.getElementById('copyright').innerHTML = `By unknown`;
-    }
+    /*for (let j = 0; j < size.length; j++) {
+        if (data.collection.items[j].data[0].photographer === undefined) {
+            document.getElementById('copyright').innerText = `By unknown`;
+        } 
+        if (data.collection.items[j].data[0].innerText === undefined) {
+            document.getElementById('location').innerHTML = `Location: unknown`;
+        }
+    }*/
+
+    document.write(`
+    <a class="my-3 btn btn-primary" href="https://www.nasa.gov/multimedia/imagegallery/index.html">Visit Nasa image gallery</a>
+    <script src="main.js"></script>
+    </body`);
 }
-});
+
